a4b29cee38bfed1037cec1dbf196f8b671f11eb8
andrewjacobsen <devbeanaj@gmail.com>
2018-11-03 17:19:28 -0400

    changed some styling for the iphone size, got the mapbox working.

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYmVhbmFqIiwiYSI6ImNqbzF0dms5ajBmZG4zcHBncWJma2Q2c2wifQ.X3EhtIyCcRTA8sjtzDjRhg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -87,7 +87,7 @@ initMap = () => {
   }).addTo(newMap);
 
   updateRestaurants();
-}
+};
 /* window.initMap = () => {
   let loc = {
     lat: 40.722216,
